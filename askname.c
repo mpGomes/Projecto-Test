7a766458a28152d575cc6b58e1d102f6b3db2956
Miguel Gomes <mp.gomes89@gmail.com>
2011-03-30 15:21:55 +0100

    good commit message.

--- askname.c
+++ askname.c
@@ -1,15 +1,13 @@
+/* askname.c */
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv)
+void askname(char *first, char *last)
 {
-	char first[255], last[255];
-
 	printf("Enter your first name: ");
 	fgets(first, 255, stdin);
 	first[strlen(first)-1] = '\0'; /* remove the newline at the end */
-	printf("Hello %s!\n", name);
-	printf("commit");
-	return 0;
 
+	printf("Now enter your last name: ");
+	gets(last); /* buffer overflow? what's that? */
 }
